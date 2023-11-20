9945b5464d71233328b0cd2cc97dbadb762881f1
Yishentw0910 <144346993+Yishentw0910@users.noreply.github.com>
2023-11-20 11:10:50 +0800

    Add files via upload

--- ML3008.aspx.cs
+++ ML3008.aspx.cs
@@ -3,7 +3,41 @@
 20131022 ADD BY SS ADAM Reason.AR附追索權無須列印保證人保證書
 20150129 ADD By ChrisFu Reason. 增加 應收帳款管理同意書、支付價金申請書、讓與明細表 三支報表
 20180625 ADD BY SS ADAM REASON.事務機增加交貨與驗收證明書
+20231130 ADD 設備系統優化 增加文件 & 條件
 */
+/*
+ML3008R01 租賃契約書 營/資租事務機
+ML3008R02 租賃契約書 營租非事務機
+ML3008R03 租賃契約書 資租非事務機
+ML3008R04 分期付款契約書 分期合約書
+ML3008R05 附條件買賣契約書 分期附條買合約書
+ML3008R06 國內應收帳款收買合約書 AR附追索權合約書
+ML3008R07 國內應收帳款收買合約書 AR無附追索權合約書
+ML3008R08 交貨與驗收證明書(營/資租)
+ML3008R09 非AR保證人保證書
+ML3008R10 AR保證人保證書
+ML3008R11 訂購契約書(三方)
+ML3008R12 訂購契約書(兩方)
+ML3008R13 租賃物返還同意書
+ML3008R14 擔保品提供證書
+ML3008R15 動產抵押契約書
+ML3008R16 應收帳款債權轉讓通知書
+ML3008R17 保險切結書
+ML3008R18 應收帳款管理同意書
+ML3008R19 支付價金申請書
+ML3008R20 讓與明細表
+ML3008R21 買賣契約書
+ML3008R22 本票
+ML3008R23 指定付款同意書
+ML3008R24 全額付款確認書(兩方版本)-確認客戶已取得產權
+ML3008R25 全額付款確認書(三方版本)-客戶發票與實撥金額有落差
+ML3008R26 全額付款確認書(三方版本)-客戶訂金抵頭款
+ML3008R27 車輛動產抵押契約書
+ML3008R28 交貨與驗收證明書(分期)
+ML3008R29 切  結  書
+ML3008R30 動產擔保交易(附條件買賣) 登記申請書
+ML3008R31 動產擔保交易登記標的物明細表
+ */
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -126,8 +160,14 @@ public partial class ML30_ML3008 : System.Web.UI.Page
                 string transType = ds.Tables[0].Rows[0]["TRANSTYPE"].ToString();
 				//20130613 ADD BY ADAM Reason. 增加保險費的判斷邏輯
 				int INSURANCE = int.Parse(ds.Tables[0].Rows[0]["INSURANCE"].ToString());
-				//20130626 ADD BY ADAM Reason. 增加全部選項
-				this.ddlDocuments.Items.Add(new ListItem("全部","ML3008R00"));
+                //20231130 ADD 設備系統優化 增加條件 & 文件
+                string EXPIREPROC = ds.Tables[0].Rows[0]["EXPIREPROC"].ToString();  //期滿處理方式 01:供應商買回 02:客戶第三方買回 03:其他 04:供應商買回(批次約定)
+                string RESIDUALS = ds.Tables[0].Rows[0]["RESIDUALS"].ToString();  //殘值
+                string PAYMENT = ds.Tables[0].Rows[0]["PAYMENT"].ToString();  //有無頭期款或保證金
+                string GUARANTEEBILL = ds.Tables[0].Rows[0]["GUARANTEEBILL"].ToString();  //1:有保證人且「簽訂大本票為是」
+                string PRODCD = ds.Tables[0].Rows[0]["PRODCD"].ToString();  //案件產品別
+                //20130626 ADD BY ADAM Reason. 增加全部選項
+                this.ddlDocuments.Items.Add(new ListItem("全部","ML3008R00"));
 
                 //UPD BY VICKY 20131126 把MAINTYPE入隱藏欄位
                 hdMAINTYPE.Value = ds.Tables[0].Rows[0]["MAINTYPE"].ToString();
@@ -156,18 +196,37 @@ public partial class ML30_ML3008 : System.Web.UI.Page
                         {
                             this.ddlDocuments.Items.Add(new ListItem("合約書", "ML3008R02"));
                             this.ddlDocuments.Items.Add(new ListItem("交貨與驗收證明書", "ML3008R08"));
-                            this.ddlDocuments.Items.Add(new ListItem("保證人保證書", "ML3008R09"));
-							RTYPE.Value+="02,08,09,";
+                            //20231130 3.	取消「保證人保證書」，條文併入主約。
+                            //this.ddlDocuments.Items.Add(new ListItem("保證人保證書", "ML3008R09"));
+                            //RTYPE.Value+="02,08,09,";
+                            RTYPE.Value+="02,08,";
                             if (transType == "02") //三方
 							{
                                 this.ddlDocuments.Items.Add(new ListItem("設備訂購契約書", "ML3008R11"));
 								RTYPE.Value+="11,";
-							}
+                                //20231130 ADD 全額付款確認書
+                                if (PAYMENT=="1")   //「有頭期款或保證金」且為三方案件→三方版本(客戶訂金抵頭款)
+                                {
+                                    this.ddlDocuments.Items.Add(new ListItem("全額付款確認書", "ML3008R26"));
+                                    RTYPE.Value += "26,";
+                                }
+                            }
                             else//兩方
 							{
                                 this.ddlDocuments.Items.Add(new ListItem("設備訂購契約書", "ML3008R12"));
 								RTYPE.Value+="12,";
-							}
+                                //20231130 ADD 全額付款確認書
+                                if (PAYMENT == "1")   //「有頭期款或保證金」且為兩方案件→兩方版本(客戶發票與實撥金額有落差)
+                                {
+                                    this.ddlDocuments.Items.Add(new ListItem("全額付款確認書", "ML3008R25"));
+                                    RTYPE.Value += "25,";
+                                }
+                                else if (PAYMENT == "2")    //「無頭期款或保證金」且為兩方案件→三方版本(確認客戶已取得產權)。
+                                {
+                                    this.ddlDocuments.Items.Add(new ListItem("全額付款確認書", "ML3008R24"));
+                                    RTYPE.Value += "24,";
+                                }
+                            }
                             this.ddlDocuments.Items.Add(new ListItem("租賃物返還同意書", "ML3008R13"));
 							RTYPE.Value+="13,";
 							//20130613 ADD BY ADAM Reason. 增加保險費的判斷邏輯
@@ -177,6 +236,21 @@ public partial class ML30_ML3008 : System.Web.UI.Page
 								RTYPE.Value+="17,";
 							}
                         }
+                        //20231130 ADD 期滿處理方式
+                        if (EXPIREPROC == "01" || EXPIREPROC == "02") //期滿處理方式為「供應商買回」或「客戶第三方買回」
+                        {
+                            this.ddlDocuments.Items.Add(new ListItem("期滿買賣契約書", "ML3008R21"));
+                            RTYPE.Value += "21,";
+                        }
+                        //20231130 ADD 大本票
+                        if (GUARANTEEBILL == "1") //有保證人且「簽訂大本票為是」
+                        {
+                            this.ddlDocuments.Items.Add(new ListItem("期滿買賣契約書", "ML3008R22"));
+                            RTYPE.Value += "22,";
+                        }
+                        //20231130 ADD 指定付款同意書
+                        this.ddlDocuments.Items.Add(new ListItem("指定付款同意書", "ML3008R23"));
+                        RTYPE.Value += "23,";
                         break;
                     case "02":
                         if (subType == "01")   //資租事務機
@@ -198,13 +272,21 @@ public partial class ML30_ML3008 : System.Web.UI.Page
                         {
                             this.ddlDocuments.Items.Add(new ListItem("合約書", "ML3008R03"));
                             this.ddlDocuments.Items.Add(new ListItem("交貨與驗收證明書", "ML3008R08"));
-                            this.ddlDocuments.Items.Add(new ListItem("保證人保證書", "ML3008R09"));
-							RTYPE.Value+="03,08,09,";
+                            //20231130 取消「保證人保證書」，條文併入主約
+                            //this.ddlDocuments.Items.Add(new ListItem("保證人保證書", "ML3008R09"));
+                            //RTYPE.Value+="02,08,09,";
+                            RTYPE.Value += "02,08,";
                             if (transType == "02") //三方
 							{
                                 this.ddlDocuments.Items.Add(new ListItem("設備訂購契約書", "ML3008R11"));
 								RTYPE.Value+="11,";
-							}
+                                //20231130 ADD 全額付款確認書
+                                if (PAYMENT == "1")   //「有頭期款或保證金」且為三方案件→三方版本(客戶訂金抵頭款)
+                                {
+                                    this.ddlDocuments.Items.Add(new ListItem("全額付款確認書", "ML3008R26"));
+                                    RTYPE.Value += "26,";
+                                }
+                            }
                             else//兩方
 							{
                                 this.ddlDocuments.Items.Add(new ListItem("設備訂購契約書", "ML3008R12"));
@@ -218,15 +300,42 @@ public partial class ML30_ML3008 : System.Web.UI.Page
 								this.ddlDocuments.Items.Add(new ListItem("保險切結書", "ML3008R17"));
 								RTYPE.Value+="17,";
 							}
+                            //20231130 ADD 全額付款確認書
+                            if (PAYMENT == "1")   //「有頭期款或保證金」且為兩方案件→兩方版本(客戶發票與實撥金額有落差)
+                            {
+                                this.ddlDocuments.Items.Add(new ListItem("全額付款確認書", "ML3008R25"));
+                                RTYPE.Value += "25,";
+                            }
+                            else if (PAYMENT == "2")    //「無頭期款或保證金」且為兩方案件→三方版本(確認客戶已取得產權)。
+                            {
+                                this.ddlDocuments.Items.Add(new ListItem("全額付款確認書", "ML3008R24"));
+                                RTYPE.Value += "24,";
+                            }
                         }
-                        break;
+                        //20231130 ADD 殘值
+                        if (RESIDUALS != "0") //殘值不為0
+                        {
+                            this.ddlDocuments.Items.Add(new ListItem("期滿買賣契約書", "ML3008R21"));
+                            RTYPE.Value += "21,";
+                        }
+                        //20231130 ADD 大本票
+                        if (GUARANTEEBILL == "1") //有保證人且「簽訂大本票為是」
+                        {
+                            this.ddlDocuments.Items.Add(new ListItem("期滿買賣契約書", "ML3008R22"));
+                            RTYPE.Value += "22,";
+                        }
+                        //20231130 ADD 指定付款同意書
+                        this.ddlDocuments.Items.Add(new ListItem("指定付款同意書", "ML3008R23"));
+                        RTYPE.Value += "23,"; break;
                     case "03":
                         if (subType == "03")   //分期設備動保
                         {
                             this.ddlDocuments.Items.Add(new ListItem("合約書", "ML3008R04"));
-                            this.ddlDocuments.Items.Add(new ListItem("交貨與驗收證明書", "ML3008R08"));
-                            this.ddlDocuments.Items.Add(new ListItem("保證人保證書", "ML3008R09"));
-							RTYPE.Value+="04,08,09,";
+                            this.ddlDocuments.Items.Add(new ListItem("交貨與驗收證明書", "ML3008R28"));
+                            //20231130 取消「保證人保證書」，條文併入主約
+                            //this.ddlDocuments.Items.Add(new ListItem("保證人保證書", "ML3008R09"));
+                            //RTYPE.Value+="04,08,09,";
+                            RTYPE.Value+="04,08,";
                             if (transType == "02") //三方
 							{
                                 this.ddlDocuments.Items.Add(new ListItem("設備訂購契約書", "ML3008R11"));
@@ -241,11 +350,27 @@ public partial class ML30_ML3008 : System.Web.UI.Page
                             this.ddlDocuments.Items.Add(new ListItem("動產抵押契約書", "ML3008R15"));
 							RTYPE.Value+="14,15,";
 							//20130613 ADD BY ADAM Reason. 增加保險費的判斷邏輯
-							if (INSURANCE == 0)
-							{
-								this.ddlDocuments.Items.Add(new ListItem("保險切結書", "ML3008R17"));
-								RTYPE.Value+="17,";
-							}
+                            //20231130 改分期時不顯示
+							//if (INSURANCE == 0)
+							//{
+							//	this.ddlDocuments.Items.Add(new ListItem("保險切結書", "ML3008R17"));
+							//	RTYPE.Value+="17,";
+							//}
+                            //20231130 ADD 案件產品別
+                            if (PRODCD=="03")//重車
+                            {
+                                this.ddlDocuments.Items.Add(new ListItem("車輛動產抵押契約書", "ML3008R27"));
+                                RTYPE.Value += "27,";
+                            }
+                            else
+                            {
+                                this.ddlDocuments.Items.Add(new ListItem("動產擔保交易登記標的物明細表", "ML3008R31"));
+                                RTYPE.Value += "31,";
+                                this.ddlDocuments.Items.Add(new ListItem("切結書", "ML3008R29"));
+                                RTYPE.Value += "29,";
+                                this.ddlDocuments.Items.Add(new ListItem("動產擔保交易登記申請書", "ML3008R30"));
+                                RTYPE.Value += "30,";
+                            }
                         }
                         else
                         {
@@ -258,6 +383,21 @@ public partial class ML30_ML3008 : System.Web.UI.Page
                                 case "02":   //分期附條買
                                     this.ddlDocuments.Items.Add(new ListItem("合約書", "ML3008R05"));
 									RTYPE.Value+="05,";
+                                    if (PRODCD == "03")//重車
+                                    {
+                                        this.ddlDocuments.Items.Add(new ListItem("車輛動產抵押契約書", "ML3008R27"));
+                                        RTYPE.Value += "27,";
+                                    }
+                                    else
+                                    {
+                                        this.ddlDocuments.Items.Add(new ListItem("動產擔保交易登記申請書", "ML3008R30"));
+                                        RTYPE.Value += "30,";
+                                        this.ddlDocuments.Items.Add(new ListItem("切結書", "ML3008R29"));
+                                        RTYPE.Value += "29,";
+                                        this.ddlDocuments.Items.Add(new ListItem("動產擔保交易登記標的物明細表", "ML3008R31"));
+                                        RTYPE.Value += "31,";
+
+                                    }
                                     break;
                                 //20131107 ADD BY SS ADAM Reason.增加設備無設定的合約書
                                 case "04":  //設備無設定
@@ -266,8 +406,10 @@ public partial class ML30_ML3008 : System.Web.UI.Page
                                     break;
                             }
                             this.ddlDocuments.Items.Add(new ListItem("交貨與驗收證明書", "ML3008R08"));
-                            this.ddlDocuments.Items.Add(new ListItem("保證人保證書部", "ML3008R09"));
-							RTYPE.Value+="08,09,";
+                            //20231130 取消「保證人保證書」，條文併入主約
+                            //this.ddlDocuments.Items.Add(new ListItem("保證人保證書部", "ML3008R09"));
+                            //RTYPE.Value+="08,09,";
+                            RTYPE.Value+="08,";
                             if (transType == "02") //三方
 							{
                                 this.ddlDocuments.Items.Add(new ListItem("設備訂購契約書", "ML3008R11"));
@@ -279,13 +421,22 @@ public partial class ML30_ML3008 : System.Web.UI.Page
 								RTYPE.Value+="12,";
 							}
 							//20130613 ADD BY ADAM Reason. 增加保險費的判斷邏輯
-							if (INSURANCE == 0)
-							{
-								this.ddlDocuments.Items.Add(new ListItem("保險切結書", "ML3008R17"));
-								RTYPE.Value+="17,";
-							}
+                            //20231130 改分期時不顯示
+							//if (INSURANCE == 0)
+							//{
+							//	this.ddlDocuments.Items.Add(new ListItem("保險切結書", "ML3008R17"));
+							//	RTYPE.Value+="17,";
+							//}
                         }
-                        this.panPRINT.Enabled = true;
+                        //20231130 ADD 大本票
+                        if (GUARANTEEBILL == "1") //有保證人且「簽訂大本票為是」
+                        {
+                            this.ddlDocuments.Items.Add(new ListItem("期滿買賣契約書", "ML3008R22"));
+                            RTYPE.Value += "22,";
+                        }
+                        //20231130 ADD 指定付款同意書
+                        this.ddlDocuments.Items.Add(new ListItem("指定付款同意書", "ML3008R23"));
+                        RTYPE.Value += "23,"; this.panPRINT.Enabled = true;
                         break;
                     case "04":
                         if (ds.Tables[0].Rows[0]["RECOURSE"].ToString() == "Y")    //AR附追索權
@@ -293,9 +444,11 @@ public partial class ML30_ML3008 : System.Web.UI.Page
                             this.ddlDocuments.Items.Add(new ListItem("合約書", "ML3008R06"));
 							//RTYPE.Value+="06,";
                             //20131022 ADD BY SS ADAM Reason.AR附追索權無須列印保證人保證書
-                            RTYPE.Value+="06,10,";
-                            this.ddlDocuments.Items.Add(new ListItem("保證人保證書", "ML3008R10"));
-						}
+                            //RTYPE.Value+="06,10,";
+                            RTYPE.Value+="06,";
+                            //20231130 取消「保證人保證書」，條文併入主約
+                            //this.ddlDocuments.Items.Add(new ListItem("保證人保證書", "ML3008R10"));
+                        }
                         else
 						{
                             this.ddlDocuments.Items.Add(new ListItem("合約書", "ML3008R07"));
